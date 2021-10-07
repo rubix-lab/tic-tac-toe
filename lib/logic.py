67acb3d6443f7d14d2fae4412e6f29b2ddc8de06
heitzlki <kirill.heitzler@gmail.com>
2021-10-08 00:01:02 +0200

    ✨ add game logic for victory

---
+++ lib/logic.py
@@ -0,0 +1,29 @@
+from lib.board import Board
+from lib.helpers import PLAYER_ONE, PLAYER_TWO
+
+def check_count(arr):
+    if(arr.count("X") == 3):
+      return PLAYER_ONE
+    elif(arr.count("O") == 3):
+      return PLAYER_TWO
+    else:
+      return None
+  
+
+def check_winner(board: Board):
+  for row in board.rows():
+    count_res = check_count(row)
+    if count_res:
+      return count_res 
+
+  for col in board.cols():
+    count_res = check_count(col)
+    if count_res:
+      return count_res
+
+  for diagonal in board.diagonals():
+    count_res = check_count(diagonal)
+    if count_res:
+      return count_res 
+  
+  return None
