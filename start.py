2a8f02508b53cd2ad0d3fea749259f825b55407a
heitzlki <kirill.heitzler@gmail.com>
2021-10-11 14:44:18 +0200

    🚧 temp start

--- start.py
+++ start.py
@@ -1,5 +1,24 @@
+# from lib.board import Board
+# from lib.game import Game
+from lib.config import Config
+
 def start():
+  config = Config().setup()
+  # game = Game(config)
+  # board = Board()
+  # print(board)
+  # game = Game()
+  # print(game.first_player())
   pass
+
+
+# b = Board([1, "X", "X", 4, 5, "O", "O", 8, 9])
+# print(b)
+# b.update(1, PLAYER_ONE)
+# print(b)
+# b.update(9, PLAYER_TWO)
+# print(b)
+
 
 if __name__ == "__main__":
   start()
