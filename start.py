21860bac6342e3333c2f2b6c7cbe2a48bb4b3548
heitzlki <kirill.heitzler@gmail.com>
2021-10-16 00:19:16 +0200

    🎨 rename func

--- start.py
+++ start.py
@@ -2,10 +2,10 @@ from lib.board import Board
 from lib.game import Game
 from lib.config import Config
 
-def start():
-  config = Config().setup()
-  board = Board()
-  game = Game(config, board).start()
+def new_game(args_config: Config = None, args_board: Board = None):
+  config = args_config if args_config else Config().setup()
+  board = args_board if args_board else Board()
+  Game(config, board).start()
 
 if __name__ == "__main__":
-  start()
+  new_game()
