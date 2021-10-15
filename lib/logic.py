2fb97d33e5d215e44229c48c1a5464565f02f471
heitzlki <kirill.heitzler@gmail.com>
2021-10-16 00:21:41 +0200

    🚧 remake of logic

--- lib/logic.py
+++ lib/logic.py
@@ -1,29 +1,33 @@
 from lib.board import Board
-from lib.helpers import PLAYER_ONE, PLAYER_TWO
+from lib.helpers import PLAYER_ONE
+from lib.player import Player
 
-def check_count(arr):
-    if(arr.count("X") == 3):
-      return PLAYER_ONE
-    elif(arr.count("O") == 3):
-      return PLAYER_TWO
-    else:
-      return None
+def check_winner(arr, player):
+  player_symbol = "X" if player.player == PLAYER_ONE else "O"
+  if(arr.count(player_symbol) == 3):
+    return player.player
+  else:
+    return None
   
 
-def check_winner(board: Board):
-  for row in board.rows():
-    count_res = check_count(row)
-    if count_res:
-      return (count_res, row) 
+def check_victory(board: Board, player: Player):
+  for row_pos in board.rows_pos():
+    print(row_pos)# board.board[pos])
+    row = [board.board[pos] for pos in row_pos]
+    winner = check_winner(row, player)
+    if winner:
+      return (row_pos, winner) 
 
-  for col in board.cols():
-    count_res = check_count(col)
-    if count_res:
-      return (count_res, col)
+  for col_pos in board.cols_pos():
+    col = [board.board[pos] for pos in col_pos]
+    winner = check_winner(col, player)
+    if winner:
+      return (col_pos, winner)
 
-  for diagonal in board.diagonals():
-    count_res = check_count(diagonal)
-    if count_res:
-      return (count_res, diagonal) 
+  for diagonal_pos in board.diagonals_pos():
+    diagonal = [board.board[pos] for pos in diagonal_pos]
+    winner = check_winner(diagonal, player)
+    if winner:
+      return (diagonal_pos, winner) 
   
   return None
