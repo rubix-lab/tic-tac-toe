42ed5e53415b2f2116793af7bb7e86d0a25b3c58
heitzlki <kirill.heitzler@gmail.com>
2021-10-15 21:47:59 +0200

    fix import; add args and start func
    Signed-off-by: heitzlki <kirill.heitzler@gmail.com>

--- lib/game.py
+++ lib/game.py
@@ -1,5 +1,5 @@
 import random
-from lib import board
+from lib.board import Board
 from lib.config import Config
 from lib.helpers import PLAYER_ONE, PLAYER_TWO, PASS_AND_PLAY
 from lib.logic import check_winner
@@ -7,12 +7,12 @@ from lib.player import Player
 from lib.ai import Ai
 
 class Game:
-  def __init__(self, config: Config) -> None:
+  def __init__(self, config: Config, board: Board) -> None:
     self.config = config
+    self.board = board
     self.first_player = Player(self.config.beginner) if self.config.mode == PASS_AND_PLAY else Ai(self.config.beginner)
     not_beginner = PLAYER_ONE if self.config.beginner == PLAYER_ONE else PLAYER_TWO
     self.second_player = Player(not_beginner) if self.config.mode == PASS_AND_PLAY else Ai(not_beginner)
-    self.loop()
 
   def game_info(self):
     print(f"")
@@ -28,22 +28,15 @@ class Game:
 
     if(possible_victory):
       self.victory = possible_victory
+      self.board.display_victory(self.victory)
       self.game_info()
       self.restart_or_exit()
 
   def loop(self):
     self._player_move_cycle(self.first_player)
     self._player_move_cycle(self.second_player)
-    # if(self.turns%2 == 0):
-    # first_player_move = self.first_player.make_move(self.board)
-    # board.update(first_player_move)
-    # possible_victory = board.check_victory(self.first_player)
-    # if(possible_victory):
-    #   self.victory = possible_victory
-    #   self.game_info()
-    #   self.restart_or_exit() # restart / new game / exit 
-    # else:
-    #   second_player_move = self.second_player.make_move(self.board)
-
+    
+  def start(self):
+    self.loop()
 
 
