fb8e8708ccbfe4b887302ade6db795d7fe0438e3
heitzlki <kirill.heitzler@gmail.com>
2021-10-16 12:17:56 +0200

    🩹 move new_game to game

--- start.py
+++ start.py
@@ -1,11 +1,4 @@
-from lib.board import Board
-from lib.game import Game
-from lib.config import Config
-
-def new_game(args_config: Config = None, args_board: Board = None):
-  config = args_config if args_config else Config().setup()
-  board = args_board if args_board else Board()
-  Game(config, board).start()
+from lib.game import new_game
 
 if __name__ == "__main__":
   new_game()
