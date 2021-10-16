68e5adf15ca557f91d8dcc3342123f7f8f5ff270
heitzlki <kirill.heitzler@gmail.com>
2021-10-16 12:20:17 +0200

    🐛 fix list comprehension index

--- lib/logic.py
+++ lib/logic.py
@@ -12,20 +12,19 @@ def check_winner(arr, player):
 
 def check_victory(board: Board, player: Player):
   for row_pos in board.rows_pos():
-    print(row_pos)# board.board[pos])
-    row = [board.board[pos] for pos in row_pos]
+    row = [board.board[pos - 1] for pos in row_pos]
     winner = check_winner(row, player)
     if winner:
       return (row_pos, winner) 
 
   for col_pos in board.cols_pos():
-    col = [board.board[pos] for pos in col_pos]
+    col = [board.board[pos - 1] for pos in col_pos]
     winner = check_winner(col, player)
     if winner:
       return (col_pos, winner)
 
   for diagonal_pos in board.diagonals_pos():
-    diagonal = [board.board[pos] for pos in diagonal_pos]
+    diagonal = [board.board[pos - 1] for pos in diagonal_pos]
     winner = check_winner(diagonal, player)
     if winner:
       return (diagonal_pos, winner) 
