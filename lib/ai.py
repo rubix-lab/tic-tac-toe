f39ce67b325ce4c0163186281703c1ee731e2cd8
heitzlki <kirill.heitzler@gmail.com>
2021-10-16 12:33:47 +0200

    🔥 remove code comments

--- lib/ai.py
+++ lib/ai.py
@@ -5,11 +5,3 @@ class Ai:
   def make_move(self, board):
     possible_moves = board.possible_moves()
     return possible_moves[0]
-    # player_move_input = input(f"Enter a position {possible_moves}: ")
-    # tmp_player_move_input = self._check_move_input(player_move_input, possible_moves)
-    # if(tmp_player_move_input == None):
-    #   return self.make_move()
-    # else:
-    #   return tmp_player_move_input
-
-  
