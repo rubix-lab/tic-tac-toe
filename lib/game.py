285d0d0bf36c2abd6ed1e0f72102f060a8480bd0
heitzlki <kirill.heitzler@gmail.com>
2021-10-16 12:19:29 +0200

    🐛 fix second player; restart/exit; add game loop

--- lib/game.py
+++ lib/game.py
@@ -1,8 +1,4 @@
 import sys
-from typing import TYPE_CHECKING
-from lib import board
-if TYPE_CHECKING:
-  import start
 from lib.board import Board
 from lib.config import Config
 from lib.helpers import PLAYER_ONE, PLAYER_TWO, PASS_AND_PLAY, clear
@@ -21,19 +17,22 @@ class Game:
     self.config = config
     self.board = board
     self.first_player = Player(self.config.beginner) if self.config.mode == PASS_AND_PLAY else Ai(self.config.beginner)
-    not_beginner = PLAYER_ONE if self.config.beginner == PLAYER_ONE else PLAYER_TWO
+    not_beginner = PLAYER_TWO if self.config.beginner == PLAYER_ONE else PLAYER_ONE
     self.second_player = Player(not_beginner) if self.config.mode == PASS_AND_PLAY else Ai(not_beginner)
+    self.victory = None
 
   def game_info(self, winner):
-    print(f"{winner.color_str()} won!\n")
+    print(f"{self.first_player if winner == self.first_player else self.second_player} player won!\n")
     pass
 
   def _check_restart_or_exit_input(self, value):
     value = value.rstrip("\n")
     if(value.isdigit() and int(value) == 1):
-      return value
+      return 1
     elif(value.isdigit() and int(value) == 2):
-      return value
+      return 2
+    elif(value.isdigit() and int(value) == 3):
+      return 3
     else:
       return None
 
@@ -48,15 +47,18 @@ class Game:
       new_config.config_info()
       new_config.start()
       clear()
-      start.new_game(new_config)
+      new_game(new_config)
     elif(tmp_restart_or_exit == 2):
-      start.new_game()
-    else:
+      clear()
+      new_game()
+    elif(tmp_restart_or_exit == 3):
       return sys.exit()
+    else:
+      self.restart_or_exit()
 
   def _player_move_cycle(self, player):
     player_move = player.make_move(self.board)
-    self.board.update(player_move, player)
+    self.board.update(player_move, player.player)
     print(self.board)
     possible_victory = check_victory(self.board, player)
 
@@ -67,15 +69,20 @@ class Game:
       self.restart_or_exit()
 
   def loop(self):
-    if(self.board.possible_moves):
-      self._player_move_cycle(self.first_player)
-      self._player_move_cycle(self.second_player)
-    else:
-      print("No winner!\n")
-      self.restart_or_exit()
+    print(self.board)
+    while(not self.victory):
+      if(self.board.possible_moves):
+        self._player_move_cycle(self.first_player)
+        self._player_move_cycle(self.second_player)
+      else:
+        print("No winner!\n")
+        self.restart_or_exit()
 
     
   def start(self):
     self.loop()
 
-
+def new_game(args_config: Config = None, args_board: Board = None):
+  config = args_config if args_config else Config().setup()
+  board = args_board if args_board else Board()
+  Game(config, board).start()
