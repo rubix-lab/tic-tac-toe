058c4aced7fb306fb8ba5f50eb33189065eb4a24
heitzlki <kirill.heitzler@gmail.com>
2021-11-14 01:08:15 +0100

    ✨ implement minimax

--- lib/ai.py
+++ lib/ai.py
@@ -1,10 +1,10 @@
+from lib import player
 from lib.board import Board
 from lib.helpers import POSITIVE_INFINITY, NEGATIVE_INFINITY, PLAYER_ONE, scores, bcolors
-from lib.logic import check_victory
-from lib.player import Player
+from lib.logic import game_over
 
 class Ai:
-  def __init__(self, player, enemy: Player, beginner = False) -> None:
+  def __init__(self, player, enemy, beginner = False) -> None:
     self.player = player
     self.enemy = enemy
     self.beginner = beginner
@@ -18,73 +18,48 @@ class Ai:
   def symbol(self) -> str:
     return self.color_str("X" if self.beginner else "O")
 
-  def make_move(self, board):
-    a = self.best_move(board)
-    print("Move", a)
-    return a #self.best_move(board)
-    # possible_moves = board.possible_moves()
-    # return possible_moves[0]
+  def make_move(self, board: Board):
+    return self.best_move(board)
 
   def best_move(self, board: Board):
-    
     best_score = NEGATIVE_INFINITY
     best_move = None
-
     for move in board.possible_moves():
-      minimax_board = Board(board.board.copy()).update(move, self.player)
-      print(minimax_board, board.board)
-      score = self.minimax(minimax_board, len(board.possible_moves()), True)
+      new_board = Board(board.board.copy())
+      new_board.update(move, self.player)
+      score = self.minimax(new_board, 0, False)
       if(score > best_score):
-        print(score, best_move)
         best_score = score
         best_move = move
-        print(score, best_move)
     
     return best_move
+
   
-  # def minimax(self, board: Board):
-  #   # print(board.possible_moves())
-  #   # return board.possible_moves()[0]
-  #   return 1
+  def minimax(self, board: Board, depth: int, isMaximizing: bool):
+    result = game_over(board)
+    if(result):
+      if(result == self.player):
+        return 1
+      elif(result == self.enemy):
+        return -1
+      else:
+        return 0
 
-  def evaluate(self, board):
-    """
-    Function to heuristic evaluation of state.
-    :param state: the state of the current board
-    :return: +1 if the computer wins; -1 if the human wins; 0 draw
-    """
-    if check_victory(self.board, self.player):
-        score = +1
-    elif check_victory(self.board, self.enemy):
-        score = -1
+    if(isMaximizing):
+      best_score = NEGATIVE_INFINITY
+      for move in board.possible_moves():
+        minimax_board = Board(board.board.copy())
+        minimax_board.update(move, self.player)
+        score = self.minimax(minimax_board, depth + 1, False)
+        best_score = max(score, best_score)
+
+      return best_score
     else:
-        score = 0
+      best_score = POSITIVE_INFINITY
+      for move in board.possible_moves():
+        minimax_board = Board(board.board.copy())
+        minimax_board.update(move, self.enemy)
+        score = self.minimax(minimax_board, depth + 1, True)
+        best_score = min(score, best_score)
 
-    return score
-
-def minimax(self, board: Board, depth: int, isMaximizing: bool): 
-  result = check_victory(board, self.player) or check_victory(board, self.enemy)
-  if(result):
-    if(result[1] == self.player):
-      score = scores.X if self.beginner else scores.O
-    else:
-      score = scores.O if self.beginner else scores.X
-    
-    return score
-  
-  if(isMaximizing):
-    best_score = NEGATIVE_INFINITY
-    for move in board.possible_moves():
-      minimax_board = Board(board.board.copy()).update(move, self.player)
-      score = self.minimax(minimax_board, depth - 1, False)
-      if(score > best_score):
-        best_score = score
-    return best_score
-  else:
-    best_score = POSITIVE_INFINITY
-    for move in board.possible_moves():
-      minimax_board = Board(board.board.copy()).update(move, self.player)
-      score = self.minimax(minimax_board, depth - 1, True)
-      if(score < best_score):
-        best_score = score
-    return best_score
+      return best_score
